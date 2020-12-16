fb4a94085216798e4b27fd4203e2b445480c3a3b
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2020-12-17 00:58:42 +0100

    update

---
+++ .bashrc
@@ -0,0 +1,5 @@
+alias ll='ls -AlF'
+export EDITOR='nvim'
+
+export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+export LIBGL_ALWAYS_INDIRECT=1
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:neovim-ppa/stable
+
+sudo apt update
+sudo apt install tmux
+sudo apt install neovim
+sudo apt upgrade
+
+cat .bashrc >> ~/.bashrc
+
+DIR=~/".vim/pack/default/start/vim-polyglot"
+if [ -d "$DIR" ]; then
+  git -C "$DIR" pull -v
+else
+  git clone -v https://github.com/sheerun/vim-polyglot.git "$DIR"
+fi
+
+cp -rv .vim/ ~
+cp -v .tmux.conf ~
+cp -rv .config/ ~
