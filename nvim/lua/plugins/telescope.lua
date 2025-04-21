770dfef67d1b7f301da8cd5a1e2b6ee7f64ef0b9
Axedyson <28491857+Axedyson@users.noreply.github.com>
2025-04-21 19:11:45 +0200

    diagnostics in telescope now

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,7 +19,8 @@ return {
       pickers = {
         find_files = { theme = "ivy" },
         live_grep = { theme = "ivy" },
-        buffers = { theme = "ivy" }
+        buffers = { theme = "ivy" },
+        diagnostics = { theme = "ivy" }
       }
     }
     require('telescope').load_extension('fzf')
@@ -27,5 +28,6 @@ return {
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
   end
 }
