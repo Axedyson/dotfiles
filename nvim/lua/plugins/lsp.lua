a60c3d0fc9babaa5a70d5eebe66565529c687dd5
Axedyson <28491857+Axedyson@users.noreply.github.com>
2025-05-12 07:41:49 +0200

    lsp errors only on cursor line when triggering

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,7 +2,8 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     'saghen/blink.cmp',
-    {"folke/lazydev.nvim",
+    {
+      "folke/lazydev.nvim",
       ft = "lua", -- only load on lua files
       opts = {
         library = {
@@ -23,6 +24,17 @@ return {
   },
   config = function(_, opts)
     local lspconfig = require('lspconfig')
+
+    vim.diagnostic.config({ virtual_text = false })
+    vim.keymap.set('n', '<leader>e', function()
+      local diagnostics = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
+      if #diagnostics > 0 then
+        vim.diagnostic.open_float({ scope = "line" })
+      else
+        print("No diagnostics found on this line")
+      end
+    end, { desc = "Show LSP diagnostics for current line" })
+
     for server, config in pairs(opts.servers) do
       -- passing config.capabilities to blink.cmp merges with the capabilities in your
       -- opts[server].capabilities, if you've defined it
