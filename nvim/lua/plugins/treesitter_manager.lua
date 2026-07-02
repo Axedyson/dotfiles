a42c11faaf3923bc6d22ebcf34e2ed7d879a91db
Axedyson <28491857+Axedyson@users.noreply.github.com>
2026-07-02 12:53:13 +0200

    Use default built-in fewfl

--- nvim/lua/plugins/treesitter_manager.lua
+++ nvim/lua/plugins/treesitter_manager.lua
@@ -4,6 +4,9 @@ return {
   config = function()
     require("tree-sitter-manager").setup({
       auto_install = true,
+      noauto_install = {
+        "c", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc"
+      },
     })
   end
 }
