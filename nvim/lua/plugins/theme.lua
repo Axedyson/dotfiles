206064f6e7eac047cb201df4425db3f44d4c58cc
Axedyson <28491857+Axedyson@users.noreply.github.com>
2026-07-02 11:16:21 +0200

    Downgrade monokai-pro plugin as it is a bit broke now

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,7 +1,7 @@
 -- the colorscheme should be available when starting Neovim
 return {
   "loctvl842/monokai-pro.nvim",
-  version = "v2.1.3",
+  version = "v2.1.1",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
